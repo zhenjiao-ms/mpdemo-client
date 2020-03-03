bbe272fffcfcb5bae951b048bddbffa487fa422d
Zhen Jiao <zhenjiao@microsoft.com>
2020-03-03 15:28:18 +0800

    add func call

--- routes/student.js
+++ routes/student.js
@@ -6,10 +6,5 @@ router.get('/', function(req, res, next) {
   res.render('student', { who: 'Student' });
 });
 
-function done() {
-	var x=document.getElementById("donemsg")
-	x.innerHTML = "Message sent to Teacher successfully"
-	get("https://zhentestwechat.azurewebsites.net/api/Inc", null, "?code=qUzUGwLj0n8jXbgTSh/OnJm49elggJpkW8lPBy3387PSlnr6LhiupA==&name=aa");
-}
 
 module.exports = router;
